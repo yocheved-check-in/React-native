50dacb7ad6a0af369307ed6da9272966d6c53947
sagargandhi111 <38329910+sagargandhi111@users.noreply.github.com>
2021-04-27 19:51:14 +0530

    Created main screen as per new design

--- app/screen/MainScreen.js
+++ app/screen/MainScreen.js
@@ -1,72 +1,225 @@
-import * as React from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  ImageBackground,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { moderateScale } from "react-native-size-matters";
 import colors from "../config/colors";
-import Chart from "../component/Main/Chart";
-import Tabbar from "../component/Main/Tabbar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-// constants coming from context api irl.
-const exampledata = [
-  {
-    x: 1,
-    y: 3.3,
-    url: require("../assets/main/spa.png"),
-    pressedUrl: require("../assets/main/spaw.png"),
-    pressed: 0,
-  },
-  {
-    x: 2,
-    y: 3.3,
-    url: require("../assets/main/food.png"),
-    pressedUrl: require("../assets/main/foodw.png"),
-    pressed: 0,
-  },
-  {
-    x: 3,
-    y: 3.3,
-    url: require("../assets/main/house.png"),
-    pressedUrl: require("../assets/main/housew.png"),
-    pressed: 0,
-  },
-];
-
-const pieColors = [
-  colors.opacityWhite,
-  colors.opacityWhite,
-  colors.opacityWhite,
-];
-
-function MainScreen({ navigation }) {
+function MainScreen() {
+  navigation = useNavigation();
   return (
-    <>
-      <ImageBackground
-        source={require("../assets/background.jpg")}
-        style={styles.background}
-      >
-        <Chart data={exampledata} colors={pieColors} navigation />
-      </ImageBackground>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/sideicon.png")}
+          />
+          <View style={styles.midheader}>
+            <Text style={styles.headerText}>GORDONTA</Text>
+          </View>
+        </View>
+      </SafeAreaView>
 
-      <View style={styles.tabContainer}>
-        <Tabbar />
+      <View style={styles.secondcontainer}>
+        <Text style={styles.bigtext}>Welcome David,</Text>
+        <Text style={styles.smalltext}>What Would You Like to Do?</Text>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("RoomService")}
+          style={styles.RoomView}
+        >
+          <ImageBackground
+            style={styles.image}
+            source={require("../assets/backgroundgift.png")}
+          >
+            <View style={styles.smalldiv}>
+              <Image
+                style={styles.heart}
+                source={require("../assets/blueheart.png")}
+              />
+              <Text style={styles.smalldivtext}>For You</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Spa")}
+          style={styles.SpaView}
+        >
+          <ImageBackground
+            style={styles.image}
+            source={require("../assets/backgroundCoussin.png")}
+          >
+            <View style={styles.smalldiv}>
+              <Image
+                style={styles.spa}
+                source={require("../assets/spa1.png")}
+              />
+              <Text style={styles.spatext}>Spa</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("HouseKeeping")}
+          style={styles.HouseView}
+        >
+          <ImageBackground
+            style={styles.image}
+            source={require("../assets/backgroundflower.png")}
+          >
+            <View style={styles.smalldiv}>
+              <Image
+                style={styles.housekeeping}
+                source={require("../assets/housekeeping1.png")}
+              />
+              <Text style={styles.housetext}>HouseKeeping</Text>
+            </View>
+            <TouchableOpacity style={styles.TouchableOpacityStyle}>
+              <Image
+                style={styles.fabimage}
+                source={require("../assets/fabicon.png")}
+              />
+              <Text style={styles.fabtext}>Preferences</Text>
+            </TouchableOpacity>
+          </ImageBackground>
+        </TouchableOpacity>
       </View>
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    width: "100%",
-    height: "100%",
-    overflow: "hidden",
-  },
   container: {
     flex: 1,
-    position: "relative",
+  },
+  logo: {
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  headerText: {
+    color: colors.white,
+    fontSize: 20,
+    fontFamily: "roboto",
+    marginTop: 5,
+  },
+  header: {
+    width: "100%",
+    backgroundColor: colors.primary,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 17,
+  },
+  midheader: {
+    width: "80%",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  secondcontainer: {
+    width: "90%",
+    textAlign: "center",
+    alignSelf: "center",
     justifyContent: "center",
   },
-  tabContainer: {
-    flex: 1,
-    justifyContent: "flex-end",
+  bigtext: {
+    fontSize: 22,
+    color: colors.primary,
+  },
+  smalltext: {
+    fontSize: 15,
+    color: colors.primary,
+  },
+  RoomView: {
+    height: "28.5%",
+  },
+  SpaView: {
+    height: "28.5%",
+  },
+  HouseView: {
+    height: "28.5%",
+  },
+  image: {
+    resizeMode: "cover",
+    justifyContent: "center",
+    height: moderateScale(220),
+    borderRadius: 8,
+  },
+  smallimage: {
+    height: 10,
+    width: 20,
+  },
+  smalldiv: {
+    height: 90,
+    width: 130,
+    textAlign: "center",
+    alignSelf: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: colors.opacityWhite,
+    borderRadius: 10,
+  },
+  heart: {
+    height: 35,
+    width: 40,
+    alignSelf: "center",
+  },
+  smalldivtext: {
+    fontSize: 13,
+    color: colors.primary,
+    alignSelf: "center",
+  },
+  spa: {
+    height: 35,
+    width: 55,
+    alignSelf: "center",
+  },
+  spatext: {
+    fontSize: 13,
+    color: "green",
+    alignSelf: "center",
+  },
+  housekeeping: {
+    height: 45,
+    width: 51,
+    alignSelf: "center",
+  },
+  housetext: {
+    fontSize: 13,
+    alignSelf: "center",
+    color: colors.pink,
+  },
+  TouchableOpacityStyle: {
+    position: "absolute",
+    width: 64,
+    height: 64,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30,
+    backgroundColor: colors.opacityWhite,
+    borderRadius: 50,
+  },
+  fabimage: {
+    width: 30,
+    height: 30,
+  },
+  fabtext: {
+    fontSize: 8,
+    color: colors.primary,
   },
 });
+
+const mapStateToProps = (state) => {
+  return { roomServiceData: state.roomServiceData };
+};
 
 export default MainScreen;
